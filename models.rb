5a6d7bef87c869914245ac05931758ef1876b0ed
Ai Miyuki <tmicltw@gmail.com>
2019-07-11 03:20:14 +0100

    Add all search related logic

--- models.rb
+++ models.rb
@@ -1,5 +1,15 @@
 require 'active_record'
 
+# reverse mapping for the roles assigned in create_database.rb
+ROLES = {
+ 'editor' => '編著',
+ 'director' => '監修',
+ 'author' => '著',
+ 'translator' => '訳',
+ 'illustrator' => '絵',
+}
+
+
 class HoldingLocation < ActiveRecord::Base
   has_many :books
 end
@@ -9,6 +19,57 @@ class Book < ActiveRecord::Base
   has_many :authors, through: :book_authors
   has_many :notes
   belongs_to :holding_location
+
+  # this is needed for where query on the authors
+  default_scope { includes(:authors).references(:author) }
+
+  def holding_location_name
+    holding_location.name
+  end
+
+  def publication_date
+    result = publication_year.to_s
+    result += "-#{publication_month}" if !publication_month.nil? && publication_month > 0
+    result
+  end
+
+  def size
+    result = ""
+    result += "#{width}x" if !width.nil? && width > 0
+    result += "#{height}cm" if !height.nil? && height > 0
+    result
+  end
+
+  ##
+  # the methods below are class/table-level filters to search in the database
+
+  def self.filter_with(key, value)
+    case key
+    when :keyword then with_keyword(value)
+    when :title then with_title(value)
+    when :author then with_author(value)
+    when :publisher then with_publisher(value)
+    else all
+    end
+  end
+
+  def self.with_keyword(keyword)
+    with_title(keyword).or(with_author(keyword)).or(with_publisher(keyword)) \
+                       .or(where(publication_year: keyword.to_i)) \
+                       .or(where(isbn: keyword))
+  end
+
+  def self.with_title(name)
+    where(arel_table[:title].matches("%#{name}%"))
+  end
+
+  def self.with_author(name)
+    where(Author.arel_table[:full_name].matches("%#{name}%"))
+  end
+
+  def self.with_publisher(name)
+    where(arel_table[:publisher].matches("%#{name}%"))
+  end
 end
 
 class Author < ActiveRecord::Base
@@ -19,6 +80,18 @@ end
 class BookAuthor < ActiveRecord::Base
   belongs_to :book
   belongs_to :author
+
+  def formatted
+    result = author.full_name
+    if role != "author"
+      result += formatted_role
+    end
+    result
+  end
+
+  def formatted_role
+    ROLES.fetch(role, "")
+  end
 end
 
 class Note < ActiveRecord::Base
--- opac.rb
+++ opac.rb
@@ -1,31 +1,92 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'pry'
+
+require_relative 'models'
+
+
+# open opac.db
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: File.join(File.dirname(__FILE__), 'opac.db')
+)
 
 
 class Opac < Sinatra::Base
+  use Rack::Static, urls: ['/public']
+
+  set :per_page, 10
+
   configure :development do
     register Sinatra::Reloader
   end
 
-  def initialize
-    super
-    @db = SQLite3::Database.new "opac.db"
+  def param_present?(key)
+    ##
+    # Checks if the parameter has been provided
+    !params.fetch(key, "").empty?
   end
 
-  get '/' do
-    query = "select title from books where 1"
-    if ! params.fetch("keywords", "").empty?
-      query += " and title like '%#{params["keywords"]}%'" 
+  def make_search
+    ##
+    # Executes the search with the current parameters
+    # and set the @books, @page and other required variables accordingly
+    @page = params.fetch(:page, "1").to_i
+    @books = Book.all
+
+    @available_years = Book.distinct.pluck(:publication_year)
+
+    # quick search
+    params.fetch(:keyword, '').split(' ').each do |keyword|
+      @books = @books.with_keyword(keyword)
     end
-    @books = @db.execute query
-    print query
+
+    # advanced search
+    @books = @books.with_title(params[:title]) if param_present?(:title)
+    @books = @books.with_author(params[:author]) if param_present?(:author)
+    @books = @books.with_publisher(params[:publisher]) if param_present?(:publisher)
+    @books = @books.where(isbn: params[:isbn]) if param_present?(:isbn)
+    @books = @books.where(publication_year: params[:year].to_i) if param_present?(:year)
+
+    @hit_count = @books.count
+    @last_page = (@books.count / settings.per_page.to_f).ceil
+
+    @books = @books.limit(settings.per_page).offset((@page - 1) * settings.per_page)
+  end
+
+  def page_url(page_number)
+    ##
+    # Returns the current URL with the given page number
+    # e.g. if the current path is /advanced-search?page=2
+    # and the method is called with 3, it will return /advanced-search?page=3
+    request.path_info + '?' + Rack::Utils.build_nested_query(params.merge(page: page_number))
+  end
+
+  # routes
+
+  get '/' do
+    ##
+    # Index route, renders a quick search
+    make_search
     erb :index
   end
 
-  get '/free-word' do
-    erb :free_word
+  get '/advanced-search' do
+    ##
+    # Renders advanced search
+    make_search
+    erb :advanced
   end
 
+  get '/books/:id' do
+    ##
+    # Renders single book
+    @book = Book.find(params[:id])
+    erb :book
+  end
+
+  # only run if script is called directly
+  # in case it is run within CGI, this line will not be executed
   run! if app_file == $0
 end
